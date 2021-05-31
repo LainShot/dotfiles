d0cc82c9d10b1f6be63e33b648cac017f2bd99af
LainShot <19312376+LainShot@users.noreply.github.com>
2021-05-31 22:52:00 +0100

    Create install_progs_debain.sh

---
+++ install_progs_debain.sh
@@ -0,0 +1,37 @@
+echo " __    _____ _     _____ _       _____ "
+echo "|  |  |  _  |_|___|   __| |_ ___|_   _|"
+echo "|  |__|     | |   |__   |   | . | | |  "
+echo "|_____|__|__|_|_|_|_____|_|_|___| |_|  "
+echo "                                       "
+echo "======================================="
+echo "running apt update"
+sudo apt update
+echo "running upgrade"
+sudo apt-get upgrade
+echo "=================================================="
+echo "installing programs"
+echo "installing discord beta...."
+sudo snap install discord --beta
+echo "installing spotify...."
+sudo snap install spotify
+echo "installing htop"
+sudo apt install -y htop
+echo "installing screenfetch"
+sudo apt install -y screenfetch
+echo "installing vs-code"
+sudo snap install code --classic
+echo "installing git"
+sudo apt install -y git
+echo "installing dev tools"
+sudo install -y build-essential
+echo "installing firefox"
+sudo install -y firefox
+echo "installing linux-headers"
+sudo apt install linux-headers-$(uname -r)
+echo "installing nmap"
+sudo apt install nmap
+echo "installing binwalk"
+sudo apt install binwalk
+echo "installing obj dump"
+sudo apt install objdump
+echo "DONE"

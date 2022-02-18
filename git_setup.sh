b57a6498d2853daea07af44da363ee45db855c70
LainShot <19312376+LainShot@users.noreply.github.com>
2022-02-18 18:00:43 +0000

    Added git script
    added a shell script that setups the git email and name to the
    protected github one.

---
+++ git_setup.sh
@@ -0,0 +1,7 @@
+#just the script to set up git stuff
+git config --global user.email "19312376+LainShot@users.noreply.github.com"
+git config --global user.name "LainShot"
+git config --global user.email
+git config --global user.name
+echo "DONE EMAIL AND NAME GIT CONFIG SETUP"
+

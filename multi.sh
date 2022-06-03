2c49ea21b32301079d9e6448b183dd1cc8fdada6
LainShot <19312376+LainShot@users.noreply.github.com>
2022-06-03 21:21:26 +0100

    setup shell config ZSH
    setup of the shell its self is manual but running multi.sh will copy the file into the correct dir.
    then install ohmyzhs.
    
    make sure you have ZHS setup on the system and shell set in order for this all to work.

---
+++ multi.sh
@@ -0,0 +1,5 @@
+echo "time to setup the shell this should work on macos or UNIX/LIN"
+
+cp .zshrc ~/.zshrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

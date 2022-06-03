0354ccb7766c566273580a85c3d7026a83b93bad
LainShot <19312376+LainShot@users.noreply.github.com>
2022-06-03 21:11:59 +0100

    setup new tmux plugins and config for the new age :D
    added setup for making sure git is installed if i only want tmux so it clones the plugin mgr
    then re-did the config.
    
    make sure to run the pm installer MODKEY + cap I in order to install them so everything works.

--- setup_tmux_debian.sh
+++ setup_tmux_debian.sh
@@ -2,4 +2,7 @@ echo "Installing tmux"
 sudo apt install tmux
 echo "setting up tmux config file"
 cp ./.tmux.conf $HOME
+echo "making sure we have git then installing plugin mgr"
+sudo apt install git
+$ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "done you can now use tmux"
--- setup_tmux_macos.sh
+++ setup_tmux_macos.sh
@@ -2,4 +2,6 @@ echo "Installing tmux"
 brew install tmux
 echo "setting up tmux config file"
 cp ./.tmux.conf $HOME
+echo "installing plugin mgr"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "done you can now run tmux."

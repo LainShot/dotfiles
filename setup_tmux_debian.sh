1446b7f0bb7bb450babc8bf1e739c29a6e9cbd9b
LainShot <19312376+LainShot@users.noreply.github.com>
2023-06-02 22:52:57 +0100

    I am a dummy....
    turns out I did this wrong and have not done a re-install in a while to notice...

--- setup_tmux_debian.sh
+++ setup_tmux_debian.sh
@@ -4,5 +4,5 @@ echo "setting up tmux config file"
 cp ./.tmux.conf $HOME
 echo "making sure we have git then installing plugin mgr"
 sudo apt install git
-$ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "done you can now use tmux"
